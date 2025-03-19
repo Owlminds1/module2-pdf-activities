2a0012ba443f37d68a9b9ccab2de0b659d97726c
mohammad asad <asadkhan41376@gmail.com>
2025-03-19 10:36:58 +0530

    image change in positive self talk activity

--- src/app/C10-L2-A5/page.tsx
+++ src/app/C10-L2-A5/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
 type formData = {
+  userName: string;
   first: string;
   sec: string;
   third: string;
@@ -55,6 +56,7 @@ const Page = () => {
     };
 
     doc.setFontSize(12);
+    addTextWithLineBreak("", data.userName);
     addTextWithLineBreak("", data.first);
     addTextWithLineBreak("", data.sec);
     addTextWithLineBreak("", data.third);
@@ -67,14 +69,34 @@ const Page = () => {
   return (
     <div className="min-h-screen bg-white p-5">
       <h3 className="text-3xl text-black text-center pb-3">
-        Positive Self talk
+        Positive Self Talk
       </h3>
       <div className="grid grid-cols-12 rounded-lg overflow-hidden">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="col-span-7 flex items-center justify-center w-full  relative h-[520px]"
+          className="col-span-7 flex items-center justify-center w-full  relative h-[500px]"
         >
-          <Image src="/images/mirror_img.jpg" fill alt=" mirror image" />
+          <Image
+            src="/images/Self_talk.png"
+            fill
+            alt=" mirror image"
+            className="object-contain"
+          />
+          <h5 className="absolute text-md text-white border border-white top-[50px] right-[42%] min-w-[120px] text-center py-2 bg-yellow-900 rounded-[20px]  ">
+            Self Talk
+          </h5>
+          <Controller
+            name="userName"
+            control={control}
+            render={({ field }) => (
+              <textarea
+                id="userName"
+                className="absolute text-black pl-2 pt-5 bg-[#ffffff6d] top-[200px] left-[38%] min-h-[50px] rounded-[20px] outline-none border-2 border-yellow-950   placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                placeholder="Enter Your Name  "
+                {...field}
+              />
+            )}
+          />
           <Controller
             name="first"
             control={control}
@@ -140,15 +162,15 @@ const Page = () => {
             )}
           />
 
-          <button className=" mirror_btn  absolute text-white  text-xl  bg-red-500  px-10 py-2  bottom-[80px] left-[42%] rounded-[20px]   placeholder:text-center placeholder:text-slate-600 focus:border-2 hover:bg-red-600 ">
+          <button className=" mirror_btn  absolute text-white  text-xl  bg-red-500  px-10 py-2  bottom-[80px] left-[40%] rounded-[20px]   placeholder:text-center placeholder:text-slate-600 focus:border-2 hover:bg-red-600 ">
             submit
           </button>
         </form>
-        <div className="col-span-5 bg-blue-400 p-5 text-xl flex flex-col gap-4 justify-center items-start text-left">
-          <p>
+        <div className="col-span-5 bg-yellow-900 p-5 text-xl flex flex-col gap-4 justify-center items-start text-left">
+          <p className="text-white">
             {`Think deeply about what you like about yourself. Focus on your qualities or behaviour towards oneself as well as others. For example, if you help your friends, you can write, ‘I’m helpful’. If you keep your things in their proper place and tidy up, you could write ‘organised.’`}
           </p>
-          <p className="text-xl">
+          <p className="text-xl text-white">
             Write on each petal a positive thought about yourself
           </p>
         </div>
