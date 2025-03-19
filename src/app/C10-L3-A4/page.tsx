27f48947bb89f4074fd6c1ec32c6f576ae79c448
mohammad asad <asadkhan41376@gmail.com>
2025-03-19 12:45:59 +0530

    add new activity C10-L3-A4

--- src/app/C10-L3-A4/page.tsx
+++ src/app/C10-L3-A4/page.tsx
@@ -4,6 +4,33 @@ import Image from "next/image";
 import React from "react";
 import { Controller, useForm } from "react-hook-form";
 
+const listData = [
+  { name: "Cooperative", border: "#b55f49" },
+  { name: "Disciplined", border: "#5aaf65" },
+  { name: "Patient", border: "#c51385" },
+  { name: "Respectful", border: "#32658a" },
+  { name: "Determined", border: "#b55f49" },
+  { name: "Honest", border: "#5aaf65" },
+  { name: "Balanced", border: "#c51385" },
+  { name: "Punctual", border: "#32658a" },
+  { name: "Innovative​", border: "#b55f49" },
+  { name: "Motivated", border: "#5aaf65" },
+  { name: "Professional", border: "#32658a" },
+  { name: "Practical", border: "#b55f49" },
+  { name: "Responsible", border: "#32658a" },
+  { name: "Open minded", border: "#c51385" },
+  { name: "Organized", border: "#c51385" },
+  { name: "Storyteller", border: "#32658a" },
+  { name: "Likable", border: "#b55f49" },
+  { name: "Persuasive", border: "#32658a" },
+  { name: "Detail-oriented", border: "#b55f49" },
+  { name: "Focused", border: "#c51385" },
+  { name: "Genuine", border: "#5aaf65" },
+  { name: "Responsible", border: "#b55f49" },
+  { name: "Smart", border: "#5aaf65" },
+  { name: "Thoughtful", border: "#c51385" },
+];
+
 type formData = {
   userName: string;
   first: string;
@@ -11,6 +38,8 @@ type formData = {
   third: string;
   four: string;
   five: string;
+  six: string;
+  seven: string;
 };
 const Page = () => {
   const { handleSubmit, control } = useForm<formData>();
@@ -62,6 +91,8 @@ const Page = () => {
     addTextWithLineBreak("", data.third);
     addTextWithLineBreak("", data.four);
     addTextWithLineBreak("", data.five);
+    addTextWithLineBreak("", data.six);
+    addTextWithLineBreak("", data.seven);
 
     doc.save("Positive-self-talk.pdf");
   };
@@ -74,24 +105,22 @@ const Page = () => {
       <div className="grid grid-cols-12 rounded-lg overflow-hidden">
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="col-span-7 flex items-center justify-center w-full  relative h-[500px]"
+          className="col-span-7 flex items-center justify-center w-full  relative h-[520px]"
         >
           <Image
-            src="/images/Self_talk.png"
+            src="/images/Self_talk2.png"
             fill
             alt=" mirror image"
             className="object-contain"
           />
-          <h5 className="absolute text-md text-white border border-white top-[50px] right-[42%] min-w-[120px] text-center py-2 bg-yellow-900 rounded-[20px]  ">
-            Self Talk
-          </h5>
+
           <Controller
             name="userName"
             control={control}
             render={({ field }) => (
               <textarea
                 id="userName"
-                className="absolute text-black pl-2 pt-5 bg-[#ffffff6d] top-[200px] left-[38%] min-h-[50px] rounded-[20px] outline-none border-2 border-yellow-950   placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[150px] left-[38%] min-h-[50px] rounded-[20px] outline-none border-2 border-yellow-950   placeholder:text-center placeholder:text-slate-600 focus:border-2 "
                 placeholder="Enter Your Name  "
                 {...field}
               />
@@ -103,7 +132,7 @@ const Page = () => {
             render={({ field }) => (
               <textarea
                 id="first"
-                className="absolute text-black pl-2 pt-5 bg-[#ffffff6d] top-[85px] right-5 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[85px] right-5 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
                 placeholder="Enter your Thought  "
                 {...field}
               />
@@ -116,7 +145,7 @@ const Page = () => {
             render={({ field }) => (
               <textarea
                 id="sec"
-                className="absolute text-black pl-2 pt-5 bg-[#ffffff6d] top-[190px] right-5 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[190px] right-5 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
                 placeholder="Enter your Thought  "
                 {...field}
               />
@@ -129,7 +158,7 @@ const Page = () => {
             render={({ field }) => (
               <textarea
                 id="third"
-                className="absolute text-black pl-2 pt-5 bg-[#ffffff6d] top-[300px] right-5 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[300px] right-5 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
                 placeholder="Enter your Thought  "
                 {...field}
               />
@@ -142,7 +171,7 @@ const Page = () => {
             render={({ field }) => (
               <textarea
                 id="four"
-                className="absolute text-black pl-2 pt-5 bg-[#ffffff6d] top-[230px] left-3 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[230px] left-3 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
                 placeholder="Enter your Thought  "
                 {...field}
               />
@@ -155,7 +184,33 @@ const Page = () => {
             render={({ field }) => (
               <textarea
                 id="five"
-                className="absolute text-black pl-2 pt-5 bg-[#ffffff6d] top-[325px] left-3 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[325px] left-3 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                placeholder="Enter your Thought  "
+                {...field}
+              />
+            )}
+          />
+
+          <Controller
+            name="six"
+            control={control}
+            render={({ field }) => (
+              <textarea
+                id="six"
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[130px] left-3 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
+                placeholder="Enter your Thought  "
+                {...field}
+              />
+            )}
+          />
+
+          <Controller
+            name="seven"
+            control={control}
+            render={({ field }) => (
+              <textarea
+                id="seven"
+                className="absolute text-black text-center pl-2 pt-5 bg-[#ffffff6d] top-[30px] left-3 min-h-[80px] rounded-[20px] outline-none border border-black  placeholder:text-center placeholder:text-slate-600 focus:border-2 "
                 placeholder="Enter your Thought  "
                 {...field}
               />
@@ -166,13 +221,18 @@ const Page = () => {
             submit
           </button>
         </form>
-        <div className="col-span-5 bg-[#F8FCFA] border p-5 text-xl flex flex-col gap-4 justify-center items-start text-left">
-          <p className="text-yellow-950">
-            {`Think deeply about what you like about yourself. Focus on your qualities or behaviour towards oneself as well as others. For example, if you help your friends, you can write, ‘I’m helpful’. If you keep your things in their proper place and tidy up, you could write ‘organised.’`}
-          </p>
-          <p className="text-xl text-yellow-950">
-            Write on each petal a positive thought about yourself
-          </p>
+        <div className="col-span-5 border bg-[#F8FCFA] p-5 text-xl flex flex-col gap-4 justify-center items-start text-left">
+          <ul className="flex flex-wrap justify-center items-center  gap-2">
+            {listData.map((i, index) => (
+              <li
+                key={index}
+                style={{ border: `2px solid ${i.border}` }}
+                className="border text-black px-8 py-2 rounded-lg"
+              >
+                {i.name}
+              </li>
+            ))}
+          </ul>
         </div>
       </div>
     </div>
